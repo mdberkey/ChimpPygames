54c96a7d2672198a98687f03b1b14086439e75f3
mdberkey <meberkey@wisc.edu>
2021-04-28 17:53:56 -0500

    GUI update

--- GUI.py
+++ GUI.py
@@ -3,14 +3,12 @@ import sys
 import os
 import subprocess
 
-
-#sys.path.append(os.path.join("Users", "michaelberkey", "work", "ChimpPygames"))
-sys.path.append(os.path.join("/home", "pi", "Desktop", "ChimpPygames"))
+sys.path.append(os.path.join("Users", "michaelberkey", "work", "ChimpPygames"))
+#sys.path.append(os.path.join("/home", "pi", "Desktop", "ChimpPygames"))
 
 class Task:
-    def __init__(self, name, script_loc, params_loc, results_loc):
+    def __init__(self, name, params_loc, results_loc):
         self.name = name
-        self.script_loc = script_loc
         self.params_loc = params_loc
         self.results_loc = results_loc
 
@@ -39,32 +37,26 @@ class Task:
         return True
 
     def start_task(self):
+        #TODO: get better exception handling
         try:
             subprocess.call(['sh', "TrainingTaskP1.sh"], cwd="Training_Task")
         except Exception:
             print("Error in opening python script for task: " + self.name)
-            print(Exception)
 
 
 class GUI:
     def __init__(self, size=(500, 300)):
         self.size = size
 
-    def generate_GUI(self):
+    def main_menu(self):
         tasks = [
-            Task("Training 1", "Training_Task/python_scripts/TrainingTaskP1.py", "Training_Task/parametersP1.dat", "Training_Task/resultsP1.csv"),
+            Task("Training 1", "Training_Task/parametersP1.dat", "Training_Task/resultsP1.csv"),
         ]
         task_names = ["Training 1", "Training 2", "Two Choice Discrimination", "Social Stimuli as Rewards", "Match to Sample", "Delayed Match to Sample", "Oddity Testing", "Delayed Response Task"]
         task_files = ["TrainingTaskP1.sh", "TrainingTaskP2.sh", "TwoChoiceDiscrim.sh", "SocialStimuli.sh", "MathToSample.sh", "DelayedMatchToSample.sh", "OddityTesting.sh", "DelayedResponseTask.sh"]
 
-
-        task_col = [[sg.Button(task_names[i])] for i in range(len(task_names))]
-        option_col = [[sg.Button(tasks[0].name)]]
-        print(task_col)
-        other_col = []
-
-        #layout = [[sg.Text("Tasks")], [sg.Button("Global Parameters")], task_row_1, task_row_2, task_row_3, task_row_4,
-         #         [sg.VSeparator()], [sg.Button("QUIT")]]
+        task_col = [[sg.Button(tasks[i].name)] for i in range(len(tasks))]
+        option_col = [[sg.Button("QUIT")]]
 
         layout = [
             [sg.Column(task_col)],
@@ -72,10 +64,10 @@ class GUI:
             [sg.Column(option_col)],
         ]
 
-        window = sg.Window("Marm Pygames", layout, margins=self.size)
+        main_window = sg.Window("Marm Pygames", layout, margins=self.size)
         
         while True:
-            event, values = window.read()
+            event, values = main_window.read()
 
             if event == "QUIT" or event == sg.WIN_CLOSED:
                 break
@@ -84,13 +76,44 @@ class GUI:
             else:
                 for task in tasks:
                     if event == task.name:
-                        params = task.get_params()
-                        print(params)
-                        task.set_params(params)
-                        task.start_task()
+                        self.params_menu(task)
+        main_window.close()
 
-        window.close()
+    def params_menu(self, task):
+        params = task.get_params()
+        params_col = []
+        for key, value in params.items():
+            if value == 'y' or value == 'n':
+                params_col.append([sg.Text(key), sg.Checkbox('', key=key)])
+            else:
+                params_col.append([sg.Text(key), sg.InputText(value, key=key)])
+
+        params_layout = [
+            [sg.Column(params_col)],
+            [sg.Button("Back to Main Menu"), sg.Button("Confirm Parameters"), sg.Button("Start Task", key='st', visible=True)]
+        ]
+        params_window = sg.Window(task.name + " Parameters", params_layout, margins=self.size)
+        while True:
+            event, values = params_window.read()
+
+            if event == "Back to Main Menu" or event == sg.WIN_CLOSED:
+                break
+            elif event == "Set Parameters":
+                for key, value in values.items():
+                    if isinstance(value, bool):
+                        if value:
+                            values[key] = 'y'
+                        else:
+                            values[key] = 'n'
+                task.set_params(values)
+                params_window['st'].update("bruh")
+                #TODO make start task button appear after parameters have been confirmed!
+
+            elif event == "Start Task":
+                task.start_task()
+
+        params_window.close()
 
 if __name__ == "__main__":
     gui = GUI()
-    gui.generate_GUI()
+    gui.main_menu()
